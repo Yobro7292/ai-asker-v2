47cafa2ead985c1dad6c11cec80f3f66aa39c66a
Yogi Hariyani <hariyaniyogi7292@gmail.com>
2023-04-01 22:07:52 +0530

    add option property in classname

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 interface ButtonProps {
-    label: string | React.ReactElement<any>,
-    className: string,
-    clickEvent?: () => void
+  label: string | React.ReactElement<any>;
+  className?: string;
+  clickEvent?: () => void;
 }
-const Button = ({label, className, clickEvent}:ButtonProps) => {
-
+const Button = ({ label, className, clickEvent }: ButtonProps) => {
   return (
     <button
-          className={`px-6 py-3 mx-1 text-sm text-black bg-white rounded-xl font-semibold hover:bg-gray-800 hover:text-white ${className} `}
-          onClick={clickEvent}
-        >
-          {label}
-        </button>
-  )
-}
+      className={`px-6 py-3 mx-1 text-sm text-black bg-white rounded-xl font-semibold hover:bg-gray-800 hover:text-white ${className} `}
+      onClick={clickEvent}
+    >
+      {label}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
