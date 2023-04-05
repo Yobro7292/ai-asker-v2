51ef2d38eb27e88a94ffdc5f1fce542b698d353b
Yogi Hariyani <hariyaniyogi7292@gmail.com>
2023-04-05 18:06:36 +0530

    shadow lg

--- components/common/Loader/Index.tsx
+++ components/common/Loader/Index.tsx
@@ -5,7 +5,7 @@ export default function Loader() {
     <div className="w-full h-screen flex justify-center items-center absolute top-0 left-0 z-10">
       <div className="w-full h-full absolute top-0 left-0 bg-neutral-900 opacity-80"></div>
       <div className="z-50 w-full p-4 h-full flex justify-center items-center">
-        <div className="relative bg-transparent rounded-lg shadow">
+        <div className="relative bg-transparent rounded-lg shadow-lg">
           <div className="animate-spin">
             <Image
               src={"/svg/loading.svg"}
